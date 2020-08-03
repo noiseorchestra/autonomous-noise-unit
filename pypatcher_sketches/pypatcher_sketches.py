ef2aaf426a5ad53c09529110ce8ac1fbc774c3c0
sandreae <sandreae@googlemail.com>
2020-08-03 12:21:30 +0100

    Modify for pytests

--- pypatcher_sketches/pypatcher_sketches.py
+++ pypatcher_sketches/pypatcher_sketches.py
@@ -6,8 +6,16 @@ from itertools import product
 jackClient = jack.Client('MadwortAutoPatcher')
 
 
+def dedup(port_names):
+    seen = set()
+    for port_name in port_names:
+        if port_name not in seen:
+            yield port_name
+            seen.add(port_name)
+
+
 def get_unique_port_names(target_ports):
-    return list(set(map(lambda x: x.name.split(':')[0], target_ports)))
+    return list(dedup(map(lambda x: x.name.split(':')[0], target_ports)))
 
 
 def get_grouped_port_list(jackClient, identifier):
@@ -22,32 +30,13 @@ def get_grouped_port_list(jackClient, identifier):
 
     grouped_ports = []
 
-    for port in target_ports:
-        grouped_ports.append([port for port_name in unique_ports if port.name.startswith(port_name)])
+    for port_name in unique_ports:
+        grouped_ports.append([port.name for port in target_ports if port.name.startswith(port_name)])
 
     return grouped_ports
 
-# This should give us things like (maybe the nested lists could be sets?):
-#
-# [[..ffff.192.168.0.1:receive_1, ..ffff.192.168.0.1:receive_2],
-# [..ffff.192.168.0.2:receive_1],
-# [..ffff.192.168.0.3:receive_1, ..ffff.192.168.0.3:receive_2]
-# etc...]
-#
 
-
-"""SIMPLE MONO or STEREO SESSION / MAIN OUT"""
-
-def disconnect_all(jackClient, receive_ports_list):
-    # we may not need this if we go for only making new connections
-    """disconnect everything from a port"""
-    # iterate over all individual ports and get connections
-    for receive_ports in (*receive_ports_list):
-        for receive_port in receive_ports:
-            send_ports = jackClient.get_all_connections(receive_port)
-
-            for send_port in send_ports:
-                jackClient.disconnect(receive_port, send_port)
+# SIMPLE MONO or STEREO SESSION / MAIN OUT
 
 
 def is_already_connected(jackClient, receive_port, send_port):
@@ -55,22 +44,24 @@ def is_already_connected(jackClient, receive_port, send_port):
 
     connected_ports = jackClient.get_all_connections(receive_port)
     for connected_port in connected_ports:
-        if send_port.name == connected_port.name:
+        if send_port == connected_port.name:
             return True
     return False
 
 
 def connect(jackClient, receive_port, send_port):
     """connect ports if not already connected"""
-    if not is_already_connected(receive_port, send_port):
+    if not is_already_connected(jackClient, receive_port, send_port):
         jackClient.connect(receive_port, send_port)
 
 
 def connect_all(jackClient, receive_ports_list, send_ports_list):
     """Connect all receive  port to list of send ports"""
 
+    all_connections = []
+
     # create all possible connections between receive_ports and send_ports.
-    for connection in product(*receive_ports_list, *send_ports_list):
+    for connection in product(receive_ports_list, send_ports_list):
 
         # this should produce connection pairs like this:
         # [([stereo-receive-left, stereo-receive-right], [mono-send]),
@@ -80,23 +71,28 @@ def connect_all(jackClient, receive_ports_list, send_ports_list):
         receive_ports = connection[0]
         send_ports = connection[1]
         # don't connect a port to itself
-        if receive_ports[0].name.split(':')[0] == send_ports[0].name.split(':')[0]:
+        if receive_ports[0].split(':')[0] == send_ports[0].split(':')[0]:
             continue
+
+        all_connections.append(connection)
 
         # Make connections depending on stereo or mono clients
         receive_stereo = True if len(receive_ports) == 2 else False
         send_stereo = True if len(send_ports) == 2 else False
         if receive_stereo and send_stereo:
-            connect(receive_ports[0], send_ports[0])
-            connect(receive_ports[1], send_ports[1])
+            connect(jackClient, receive_ports[0], send_ports[0])
+            connect(jackClient, receive_ports[1], send_ports[1])
         if receive_stereo and not send_stereo:
-            connect(receive_ports[0], send_ports[0])
-            connect(receive_ports[1], send_ports[0])
+            connect(jackClient, receive_ports[0], send_ports[0])
+            connect(jackClient, receive_ports[1], send_ports[0])
         if not receive_stereo and not send_stereo:
-            connect(receive_ports[0], send_ports[0])
+            connect(jackClient, receive_ports[0], send_ports[0])
 
+    print(all_connections)
+    return all_connections
 
-"""PANNED LADSPA SESSION"""
+# PANNED LADSPA SESSION
+
 
 def is_already_connected_ladspa(jackClient, receive_port, ladspa_send_ports):
     """check if ports are already connected to ANY ladspa send"""
@@ -135,19 +131,19 @@ def connect_all_to_ladspa(jackClient, receive_ports_list, ladspa_send_ports):
 
 # make lists of all ports we want to connect
 
-jacktrip_client_receives = get_grouped_port_list(jackClient, 'receive')
-jacktrip_client_sends = get_grouped_port_list(jackClient, 'send')
-darkice_sends = jackClient.get_ports('.*darkice.*')
-ladspa_sends = jackClient.get_ports('.*Input.*')
-ladspa_receives = jackClient.get_ports('.*Output.*')
+# jacktrip_client_receives = get_grouped_port_list(jackClient, 'receive')
+# jacktrip_client_sends = get_grouped_port_list(jackClient, 'send')
+# darkice_sends = jackClient.get_ports('.*darkice.*')
+# ladspa_sends = jackClient.get_ports('.*Input.*')
+# ladspa_receives = jackClient.get_ports('.*Output.*')
 
 # make all mono / stereo connections
-connect_all(jackClient, jacktrip_client_receives, [jacktrip_client_sends, darkice_sends])
+# connect_all(jackClient, jacktrip_client_receives, [jacktrip_client_sends, darkice_sends])
 
 # OR
 
 # make all panned ladspa connections
-connect_all_to_ladspa(jackClient, jacktrip_client_receives, ladspa_sends)
+# connect_all_to_ladspa(jackClient, jacktrip_client_receives, ladspa_sends)
 
 # then connect ladspa to all desired sends in stereo
 # TO DO
