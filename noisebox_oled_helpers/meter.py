cc74f645c7bce3073663e2fcae6a84ae20504d3c
sandreae <sandreae@googlemail.com>
2020-08-18 14:28:07 +0100

    Position level_meter channel name

--- noisebox_oled_helpers/meter.py
+++ noisebox_oled_helpers/meter.py
@@ -6,7 +6,7 @@ def vertical_bar(draw, name, x1, y1, x2, y2, yh):
     """Draw meter bar and frame"""
     draw.rectangle((x1, y1) + (x2, y2), "black", "white")
     draw.rectangle((x1, y1) + (x2, yh), "white", "white")
-    draw.text((x1 + 4, y1 + 2), name, fill="white")
+    draw.text((x1, y1 + 2), name, fill="white")
 
 
 def render(draw, name, width, height, meter):
