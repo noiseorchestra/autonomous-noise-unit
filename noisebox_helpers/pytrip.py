93ca30eaf62b6e27b99c93a28223044f45e595ee
sandreae <sandreae@googlemail.com>
2020-08-21 10:00:52 +0100

    Create command for starting peer JackTrip sessions

--- noisebox_helpers/pytrip.py
+++ noisebox_helpers/pytrip.py
@@ -7,19 +7,30 @@ class PyTrip:
         self.current_jacktrip = None
         self.connected = False
 
-    def generate_command(self, params):
+    def generate_client_command(self, params):
         """Generate JackTrip command"""
 
         ip = params["ip"]
         n = "-n" + params["channels"]
         q = "-q" + params["queue"]
+        server_type = "-C" if params["hub_mode"] else "-c"
 
-        return ["jacktrip", "-C", ip, n, q, "-z"]
+        return ["jacktrip", server_type, ip, n, q, "-z"]
 
-    def start(self, params):
+    def generate_server_command(self, params):
+        """Generate JackTrip command"""
+
+        n = "-n" + params["channels"]
+        q = "-q" + params["queue"]
+
+        return ["jacktrip", "-s", n, q, "-z"]
+
+    def start(self, params, server=False):
         """Start JackTrip with relevent parameters"""
 
-        command = self.generate_command(params)
+        command = self.generate_client_command(params)
+        if server is True:
+            command = self.generate_server_command(params)
         self.current_jacktrip = Popen(command, stdout=PIPE, stderr=STDOUT)
 
     def stop(self):
