f0d3ee4249372ff6f0bda1b19758430208f88ade
sandreae <sandreae@googlemail.com>
2020-09-07 12:13:43 +0100

    Test UPDATE message

--- noisebox.py
+++ noisebox.py
@@ -165,6 +165,7 @@ def main():
 
     menu_items = ['CONNECT TO SERVER',
                   'LEVEL METER',
+                  'HELLO DAVE!',
                   'P2P SESSION',
                   'SETTINGS -->']
 
