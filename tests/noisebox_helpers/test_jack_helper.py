f4fd81502149a44cca376df281d175771aa69b8f
sandreae <sandreae@googlemail.com>
2020-10-12 11:15:04 +0100

    Fix name

--- tests/noisebox_helpers/test_jack_helper.py
+++ tests/noisebox_helpers/test_jack_helper.py
@@ -5,7 +5,7 @@ jack_helper = nh.jack_helper.JackHelper()
 
 local_receive_ports = ["system:capture_1", "system:capture_2"]
 
-def test_process_meter_value():
+def test_check_input_ports():
     with pytest.raises(nh.NoiseBoxCustomError):
         assert jack_helper.check_input_ports([], stereo=True) == []
     assert jack_helper.check_input_ports(local_receive_ports, stereo=True) == local_receive_ports
