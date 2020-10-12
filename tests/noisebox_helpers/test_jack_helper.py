67a5ce23d9700cbc3961bd298f9b9d3d146ff958
sandreae <sandreae@googlemail.com>
2020-10-12 11:54:26 +0100

    Write tests for set_all_connections in jack_helper

--- tests/noisebox_helpers/test_jack_helper.py
+++ tests/noisebox_helpers/test_jack_helper.py
@@ -10,3 +10,35 @@ def test_check_input_ports():
         assert jack_helper.check_input_ports([], stereo=True) == []
     assert jack_helper.check_input_ports(local_receive_ports, stereo=True) == local_receive_ports
     assert jack_helper.check_input_ports(local_receive_ports, stereo=False) == [local_receive_ports[0]]
+
+def test_set_all_connections_monitoring_stereo_stereo():
+    receive_ports_list = [
+        ["system:capture_01", "system:capture_02"]
+    ]
+    send_ports_list = [
+        ["system:playback_01", "system:playback_02"]
+    ]
+    jack_helper.set_all_connections(receive_ports_list, send_ports_list)
+    assert jack_helper.connections == [('system:capture_01', 'system:playback_01'),
+                                       ('system:capture_02', 'system:playback_02')]
+
+def test_set_all_connections_monitoring_mono_stereo():
+    receive_ports_list = [
+        ["system:capture_01"]
+    ]
+    send_ports_list = [
+        ["system:playback_01", "system:playback_02"]
+    ]
+    jack_helper.set_all_connections(receive_ports_list, send_ports_list)
+    assert jack_helper.connections == [('system:capture_01', 'system:playback_01'),
+                                       ('system:capture_01', 'system:playback_02')]
+
+def test_set_all_connections_monitoring_mono_mono():
+    receive_ports_list = [
+        ["system:capture_01"]
+    ]
+    send_ports_list = [
+        ["system:playback_01"]
+    ]
+    jack_helper.set_all_connections(receive_ports_list, send_ports_list)
+    assert jack_helper.connections == [('system:capture_01', 'system:playback_01')]
