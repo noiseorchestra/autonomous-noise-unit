2c0ac317c6a6742ff2c546b98adbd1b846092f67
sandreae <sandreae@googlemail.com>
2020-10-18 17:09:09 +0100

    Typo

--- noisebox_oled_helpers/menu.py
+++ noisebox_oled_helpers/menu.py
@@ -8,7 +8,7 @@ class Menu(MenuItems):
     """Class for drawing OLED menu"""
 
     def __init__(self):
-        super()._init_()
+        super().__init__()
         self.counter = 0
         self.menuindex = 0
         self.device = None
