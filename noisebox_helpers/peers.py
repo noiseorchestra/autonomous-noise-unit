934d0b73176730b2a18cb1e99e5f2d349eb39e59
sandreae <sandreae@googlemail.com>
2020-10-23 14:01:27 +0100

    Increase thread pool

--- noisebox_helpers/peers.py
+++ noisebox_helpers/peers.py
@@ -26,7 +26,7 @@ def ping_peer(peer):
 def ping_all(peers):
     """Check status of all peers and return result"""
 
-    pool = ThreadPoolExecutor(10)
+    pool = ThreadPoolExecutor(20)
     pings = []
     results = []
 
