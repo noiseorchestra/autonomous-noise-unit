8a154c9eb170b0714bab651904a2b740e16e19ce
sandreae <git@contact.samandreae.com>
2021-01-06 19:32:40 +0000

    clone into home directory FIX

--- update_jacktrip.sh
+++ update_jacktrip.sh
@@ -5,9 +5,10 @@ sudo apt-get -y install librtaudio-dev
 # Build JackTrip from source
 
 sudo apt-get -y install qt5-default qt5-qmake
+cd ~/
 sudo rm -r jacktrip
-git clone https://github.com/jacktrip/jacktrip.git ~/jacktrip
-cd ~/jacktrip/src
+git clone https://github.com/jacktrip/jacktrip.git
+cd jacktrip/src
 git checkout beta.rc.1.3.0
 qmake jacktrip.pro
 sudo make install
