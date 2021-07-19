9983f997509cab6b3d4dfe460cca0e0b28c9c8aa
sandreae <git@contact.samandreae.com>
2021-07-19 07:31:03 +0100

    Don't build vpn ip from hostname

--- noisebox_helpers/get_ip.py
+++ noisebox_helpers/get_ip.py
@@ -8,7 +8,6 @@ def ip_address():
     s.connect(("8.8.8.8", 80))
     ip = (s.getsockname()[0])
     sock_name = socket.gethostname()
-    vpn_ip = "10.0.0." + sock_name.strip("noisebox")
     external_ip = urllib.request.urlopen('https://ident.me').read().decode('utf8')
     s.close()
-    return [sock_name, external_ip, vpn_ip, str(ip)]
+    return [sock_name, external_ip, str(ip)]
